77ec47e692c3d96c74a9da8b16c324fd0b1c6763
Moacyr <moacyrpereira10@hotmail.com>
2021-10-25 11:04:28 -0300

    SellerService e FindAll

--- VendasWeb/Startup.cs
+++ VendasWeb/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using VendasWeb.Models;
 using VendasWeb.Data;
+using VendasWeb.Services;
 
 namespace VendasWeb
 {
@@ -42,6 +43,7 @@ namespace VendasWeb
                         builder.MigrationsAssembly("VendasWeb")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
