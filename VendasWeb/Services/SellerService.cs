3b4542d9d0a1b3d575f622c001cb6e83239ac69b
Moacyr <moacyrpereira10@hotmail.com>
2021-10-26 12:31:51 -0300

    Página para editar vendedor e exceções de serviço personalizadas

--- VendasWeb/Services/SellerService.cs
+++ VendasWeb/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using VendasWeb.Models;
 using Microsoft.EntityFrameworkCore;
+using VendasWeb.Services.Exceptions;
 
 namespace VendasWeb.Services
 {
@@ -22,7 +23,7 @@ namespace VendasWeb.Services
         }
 
         public void Insert(Seller obj)
-        {   
+        {
             _context.Add(obj);
             _context.SaveChanges();
         }
@@ -37,6 +38,23 @@ namespace VendasWeb.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id Não encontrado");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
