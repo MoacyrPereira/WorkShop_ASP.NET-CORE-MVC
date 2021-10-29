9bb8a23c16f146d798b99d63066dc5ed7130ff81
Moacyr <moacyrpereira10@hotmail.com>
2021-10-29 13:44:40 -0300

    Implementado Busca agrupada

--- VendasWeb/Models/SalesRecord.cs
+++ VendasWeb/Models/SalesRecord.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using VendasWeb.Models.Enums;
 
 namespace VendasWeb.Models
@@ -6,8 +7,16 @@ namespace VendasWeb.Models
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
+
+
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
 
